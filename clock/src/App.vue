9bc17a88f25cb83a4cb827558faee61838987c94
Haider <haideryouxd4@gmail.com>
2021-07-26 16:12:47 +0500

    clock

---
+++ clock/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <DigitalClock color="yellow" />
+  </div>
+</template>
+
+<script>
+import DigitalClock from "./components/DigitalClock.vue";
+export default {
+  name: "App",
+  components: {
+    DigitalClock,
+  },
+};
+</script>
+<style></style>
